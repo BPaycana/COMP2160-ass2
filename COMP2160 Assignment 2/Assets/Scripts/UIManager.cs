60a9737214f92d2b89f8b2c6f535a8a41b23232f
BPaycana <earl.paycana@students.mq.edu.au>
2021-10-29 21:44:57 +1100

    Health Bar v0.1
    Added Health Bar, not attached to anything yet but it works.

---
+++ COMP2160 Assignment 2/Assets/Scripts/UIManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+    // public static UIManager instance;
+    // public GameObject losePanel;
+    // public GameObject winPanel;
+
+    // private bool paused = false;
+
+    // private void Awake()
+    // {
+    //     instance = this;
+    // }
+
+    // void Start()
+    // {
+    //     Unpause();
+    // }
+
+    // void Update()
+    // {
+        
+    // }
+
+    // public void GameOver()
+    // {
+    //     paused = true;
+    //     losePanel.SetActive(true);
+    //     Time.timeScale = 0;
+    // }
+
+    // public void Unpause()
+    // {
+    //     paused = false;
+    //     losePanel.SetActive(false);
+    //     winPanel.SetActive(false);
+    //     Time.timeScale = 1;
+    // }
+}
